4dda841059600bb10fee25f0798f0e37b1766c89
Tianshu Qiu <tian.shu.qiu@intel.com>
2015-05-12 14:54:45 +0800

    atomisp2: Add handling for V4L2_CID_EXPOSURE_AUTO
    This patch adds handling of V4L2_CID_EXPOSURE_AUTO
    for atomisp2. This control is forwarded to sensor
    driver.
    
    Change-Id: Ibc205f0e73cc61aec42215d179a603b29bcaf869
    Fixes-Issue: #JIMINAN-35982
    Signed-off-by: Tianshu Qiu <tian.shu.qiu@intel.com>
    Reviewed-on: https://icggerrit.ir.intel.com/55553
    Build: IMIN (SC)
    Validation-Android: Wei Tao <wei.tao@intel.com>
    Approver: Bin B Han <bin.b.han@intel.com>
    Reviewed-by: Bin B Han <bin.b.han@intel.com>
    Integrator: WeiX Meng <weix.meng@intel.com>
    Build: WeiX Meng <weix.meng@intel.com>
    Reviewed-by: WeiX Meng <weix.meng@intel.com>
    Maintainer: WeiX Meng <weix.meng@intel.com>

--- drivers/external_drivers/camera/drivers/media/pci/atomisp2/atomisp_driver/atomisp_ioctl.c
+++ drivers/external_drivers/camera/drivers/media/pci/atomisp2/atomisp_driver/atomisp_ioctl.c
@@ -2206,6 +2206,7 @@ static int atomisp_g_ctrl(struct file *file, void *fh,
 	case V4L2_CID_2A_STATUS:
 	case V4L2_CID_AUTO_N_PRESET_WHITE_BALANCE:
 	case V4L2_CID_EXPOSURE:
+	case V4L2_CID_EXPOSURE_AUTO:
 	case V4L2_CID_SCENE_MODE:
 	case V4L2_CID_ISO_SENSITIVITY:
 	case V4L2_CID_ISO_SENSITIVITY_AUTO:
@@ -2279,6 +2280,7 @@ static int atomisp_s_ctrl(struct file *file, void *fh,
 	switch (control->id) {
 	case V4L2_CID_AUTO_N_PRESET_WHITE_BALANCE:
 	case V4L2_CID_EXPOSURE:
+	case V4L2_CID_EXPOSURE_AUTO:
 	case V4L2_CID_EXPOSURE_AUTO_PRIORITY:
 	case V4L2_CID_SCENE_MODE:
 	case V4L2_CID_ISO_SENSITIVITY:
@@ -2384,6 +2386,7 @@ static int atomisp_camera_g_ext_ctrls(struct file *file, void *fh,
 		ctrl.value = c->controls[i].value;
 		switch (ctrl.id) {
 		case V4L2_CID_EXPOSURE_ABSOLUTE:
+		case V4L2_CID_EXPOSURE_AUTO:
 		case V4L2_CID_IRIS_ABSOLUTE:
 		case V4L2_CID_FNUMBER_ABSOLUTE:
 		case V4L2_CID_BIN_FACTOR_HORZ:
@@ -2493,6 +2496,7 @@ static int atomisp_camera_s_ext_ctrls(struct file *file, void *fh,
 		ctrl.value = c->controls[i].value;
 		switch (ctrl.id) {
 		case V4L2_CID_EXPOSURE_ABSOLUTE:
+		case V4L2_CID_EXPOSURE_AUTO:
 		case V4L2_CID_EXPOSURE_METERING:
 		case V4L2_CID_IRIS_ABSOLUTE:
 		case V4L2_CID_FNUMBER_ABSOLUTE:
