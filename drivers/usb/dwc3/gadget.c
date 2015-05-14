28d9e5d2cb5b409bd867d7503e5f7e714c9d1f77
xiao jin <jin.xiao@intel.com>
2015-05-14 19:11:59 +0800

    dwc3 gadget.c: add dequeue_forced to avoid force END command
    Dwc3 driver may send END command at dequeue if the req
    has been START. But hardware can't process END command
    clearly and usb component might stop working after END
    command.
    
    We can't send END command in this case. We add a callback
    dequeue_forced. If parameter forced=0, dequeue_forced can
    return -EBUSY and caller need wait for some time to make
    sure hardware finishes the request.
    
    Change-Id: I2c0f94b77d65fc8c8594ba7bc728d93c5fba3320
    Tracked-On: https://jira01.devtools.intel.com/browse/AKP-1561
    Signed-off-by: xiao jin <jin.xiao@intel.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1196,8 +1196,9 @@ static int dwc3_gadget_ep_queue(struct usb_ep *ep, struct usb_request *request,
 	return ret;
 }
 
-static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
-		struct usb_request *request)
+static int dwc3_gadget_ep_dequeue_forced(struct usb_ep *ep,
+		struct usb_request *request,
+		int forced)
 {
 	struct dwc3_request		*req = to_dwc3_request(request);
 	struct dwc3_request		*r = NULL;
@@ -1221,9 +1222,14 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 				break;
 		}
 		if (r == req) {
-			/* wait until it is processed */
-			dwc3_stop_active_transfer(dwc, dep->number);
-			goto out1;
+			if (!forced) {
+				ret = -EBUSY;
+				goto out0;
+			} else {
+				/* wait until it is processed */
+				dwc3_stop_active_transfer(dwc, dep->number);
+				goto out1;
+			}
 		}
 		dev_err(dwc->dev, "request %p was not queued to %s\n",
 				request, ep->name);
@@ -1239,6 +1245,12 @@ out0:
 	spin_unlock_irqrestore(&dwc->lock, flags);
 
 	return ret;
+}
+
+static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
+		struct usb_request *request)
+{
+	return dwc3_gadget_ep_dequeue_forced(ep, request, 1);
 }
 
 int __dwc3_gadget_ep_set_halt(struct dwc3_ep *dep, int value, int protocol)
@@ -1335,6 +1347,7 @@ static const struct usb_ep_ops dwc3_gadget_ep0_ops = {
 	.free_request	= dwc3_gadget_ep_free_request,
 	.queue		= dwc3_gadget_ep0_queue,
 	.dequeue	= dwc3_gadget_ep_dequeue,
+	.dequeue_forced	= dwc3_gadget_ep_dequeue_forced,
 	.set_halt	= dwc3_gadget_ep0_set_halt,
 	.set_wedge	= dwc3_gadget_ep_set_wedge,
 };
@@ -1346,6 +1359,7 @@ static const struct usb_ep_ops dwc3_gadget_ep_ops = {
 	.free_request	= dwc3_gadget_ep_free_request,
 	.queue		= dwc3_gadget_ep_queue,
 	.dequeue	= dwc3_gadget_ep_dequeue,
+	.dequeue_forced	= dwc3_gadget_ep_dequeue_forced,
 	.set_halt	= dwc3_gadget_ep_set_halt,
 	.set_wedge	= dwc3_gadget_ep_set_wedge,
 };
