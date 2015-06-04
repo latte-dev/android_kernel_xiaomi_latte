afcbbaca15c583b24a3eac6e75fc7bdfac354719
Zaikuo Wang <zaikuo.wang@intel.com>
2015-06-04 16:37:34 +0800

    ov5693: add 1280*960 for CATS video FOV test
    Change-Id: Ib5fec2e67d1a91e6e0797270e3926241b6b5201b
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-33855
    Signed-off-by: Zaikuo Wang <zaikuo.wang@intel.com>
    Reviewed-on: https://icggerrit.ir.intel.com/58883
    Build: IMIN (SC)
    Approver: Yunliang Ding <yunliang.ding@intel.com>
    Reviewed-by: Yunliang Ding <yunliang.ding@intel.com>
    Integrator: WeiX Meng <weix.meng@intel.com>
    Build: WeiX Meng <weix.meng@intel.com>
    Reviewed-by: WeiX Meng <weix.meng@intel.com>
    Maintainer: WeiX Meng <weix.meng@intel.com>

--- drivers/external_drivers/camera/drivers/media/i2c/ov5693-ecs/ov5693.h
+++ drivers/external_drivers/camera/drivers/media/i2c/ov5693-ecs/ov5693.h
@@ -549,6 +549,50 @@ static struct ov5693_reg const ov5693_654x496[] = {
 };
 
 /*
+ * 1296x976 30fps 17ms VBlanking 2lane 10Bit (Scaling)
+*DS from 2624x1492
+*/
+static struct ov5693_reg const ov5693_1296x976[] = {
+	{OV5693_8BIT, 0x3501, 0x7b},
+	{OV5693_8BIT, 0x3502, 0x00},
+	{OV5693_8BIT, 0x3708, 0xe2},
+	{OV5693_8BIT, 0x3709, 0xc3},
+
+	{OV5693_8BIT, 0x3800, 0x00},
+	{OV5693_8BIT, 0x3801, 0x00},
+	{OV5693_8BIT, 0x3802, 0x00},
+	{OV5693_8BIT, 0x3803, 0x00},
+
+	{OV5693_8BIT, 0x3804, 0x0a},
+	{OV5693_8BIT, 0x3805, 0x3f},
+	{OV5693_8BIT, 0x3806, 0x07},
+	{OV5693_8BIT, 0x3807, 0xA3},
+
+	{OV5693_8BIT, 0x3808, 0x05},
+	{OV5693_8BIT, 0x3809, 0x10},
+	{OV5693_8BIT, 0x380a, 0x03},
+	{OV5693_8BIT, 0x380b, 0xD0},
+
+	{OV5693_8BIT, 0x380c, 0x0a},
+	{OV5693_8BIT, 0x380d, 0x80},
+	{OV5693_8BIT, 0x380e, 0x07},
+	{OV5693_8BIT, 0x380f, 0xc0},
+
+	{OV5693_8BIT, 0x3813, 0xE8},
+
+	{OV5693_8BIT, 0x3814, 0x11},	/*X subsample control*/
+	{OV5693_8BIT, 0x3815, 0x11},	/*Y subsample control*/
+	{OV5693_8BIT, 0x3820, 0x00},
+	{OV5693_8BIT, 0x3821, 0x1e},
+	{OV5693_8BIT, 0x5002, 0x00},
+	{OV5693_8BIT, 0x5041, 0x84}, /* scale is auto enabled */
+	{OV5693_8BIT, 0x0100, 0x01},
+	{OV5693_TOK_TERM, 0, 0}
+
+};
+
+
+/*
  * 336x256 30fps 17ms VBlanking 2lane 10Bit (Scaling)
  DS from 2564x1956
  */
@@ -1259,6 +1303,20 @@ struct ov5693_resolution ov5693_res_video[] = {
 		.bin_factor_y = 2,
 		.bin_mode = 0,
 		.regs = ov5693_1296x736,
+	},
+	{
+		.desc = "ov5693_1296x976_30fps",
+		.width = 1296,
+		.height = 976,
+		.fps = 30,
+		.pix_clk_freq = 160,
+		.used = 0,
+		.pixels_per_line = 2688,
+		.lines_per_frame = 1984,
+		.bin_factor_x = 2,
+		.bin_factor_y = 2,
+		.bin_mode = 0,
+		.regs = ov5693_1296x976,
 	},
 	{
 		.desc = "ov5693_1636P_30fps",
