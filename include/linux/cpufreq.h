fa4dcb6706ad636bca0130a477fa3240a39c7a03
xinhui.pan <xinhuix.pan@intel.com>
2015-08-04 10:40:24 +0800

    cpufreq: Add support for per-policy driver data
    Drivers supporting multiple clusters or multiple 'struct cpufreq_policy'
    instances may need to keep per-policy data. If the core doesn't provide support
    for that, they might do it in the most unoptimized way: 'per-cpu' data.
    
    This patch adds another field in struct cpufreq_policy: 'driver_data'. It isn't
    accessed by core and is for driver's internal use only.
    
    Change-Id: Ie9025393ad80dbdbc0be0b479409ac61a7e386aa
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-42456
    Tracked-On: https://jira01.devtools.intel.com/browse/AKP-2217
    Tested-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -103,6 +103,8 @@ struct cpufreq_policy {
 	 *     __cpufreq_governor(data, CPUFREQ_GOV_POLICY_EXIT);
 	 */
 	struct rw_semaphore	rwsem;
+	/* For cpufreq driver's internal use */
+	void			*driver_data;
 };
 
 /* Only for ACPI */
