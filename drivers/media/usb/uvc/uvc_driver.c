6807eb263ac59fded4e4041a1ad2153b6f5d8a8c
Aviv Greenberg <aviv.d.greenberg@intel.com>
2015-09-15 16:00:33 -0700

    UVC: Add support sr300 depth camera
    Add support for Intel SR300 depth camera in uvc driver.
    This includes adding new uvc GUIDs for the new pixel formats,
    adding new V4L pixel format definition to user api headers,
    and updating the uvc driver GUID-to-4cc tables with the new formats.
    
    Change-Id: Ice5142799434bc62c4a850c75126368cc40826d1
    Signed-off-by: Aviv Greenberg <aviv.d.greenberg@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-18584

--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -142,6 +142,21 @@ static struct uvc_format_desc uvc_fmts[] = {
 		.guid		= UVC_GUID_FORMAT_RW10,
 		.fcc		= V4L2_PIX_FMT_SRGGB10P,
 	},
+	{
+		.name		= "Depth data 16-bit (Z16)",
+		.guid		= UVC_GUID_FORMAT_INVZ,
+		.fcc		= V4L2_PIX_FMT_Z16,
+	},
+	{
+		.name		= "Depth:IR 26-bit (INZI)",
+		.guid		= UVC_GUID_FORMAT_INZI,
+		.fcc		= V4L2_PIX_FMT_INZI,
+	},
+	{
+		.name		= "Infrared 10-bit (INVI)",
+		.guid		= UVC_GUID_FORMAT_INVI,
+		.fcc		= V4L2_PIX_FMT_INVI,
+	},
 };
 
 /* ------------------------------------------------------------------------
