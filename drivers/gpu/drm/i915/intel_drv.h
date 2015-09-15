189313787bd02555a8881eaa472b083819e36cde
Thulasimani,Sivakumar <sivakumar.thulasimani@intel.com>
2015-09-15 20:15:27 +0530

    FOR_UPSTREAM [VPG]: drm/i915: Calculate multiplier value for DP panels
    Multiplier values are calculated based on the link rate used for DP
    panel. This patch adds this capability so we can use the correct value
    for clock programming.
    
    v2: use #defines for magic numbers
    
    Change-Id: Iefee3bac93025f7c1676537c439c268161d887ba
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-5971
    Signed-off-by: Sivakumar Thulasimani <sivakumar.thulasimani@intel.com>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -1005,6 +1005,9 @@ bool intel_dp_compute_config(struct intel_encoder *encoder,
 bool intel_dp_is_edp(struct drm_device *dev, enum port port);
 bool intel_dp_hpd_pulse(struct intel_digital_port *intel_dig_port,
 			bool long_hpd);
+uint8_t intel_dp_calc_multiplier(struct intel_encoder *encoder,
+			u32 pixel_clock);
+
 void intel_edp_backlight_on(struct intel_dp *intel_dp);
 void intel_edp_backlight_off(struct intel_dp *intel_dp);
 void intel_edp_panel_vdd_on(struct intel_dp *intel_dp);
