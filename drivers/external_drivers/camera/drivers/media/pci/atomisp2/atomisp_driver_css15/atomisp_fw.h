14ead0bb66c7626f59cb5de9b79be9c4218c99ff
Zhang, Dongdong <dongdong.zhang@intel.com>
2015-12-17 21:08:49 +0800

    Fix license issue
    Change-Id: Ifa2a2a50a8a0e8cc42048100a7373610de14cf22
    Original-Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-47288
    Signed-off-by: Zhang, Dongdong <dongdong.zhang@intel.com>
    Reviewed-on: https://icggerrit.ir.intel.com/90615
    Build: Dessert M BXT RVP (SC)
    Build: Dessert M CHT FFD (SC)
    Validation-Android: Dessert M CHT FFD (SC)
    Approver: Fan F Yang <fan.f.yang@intel.com>
    Integrator: WeiX Meng <weix.meng@intel.com>
    Approver: Hongjiang A Zheng <hongjiang.a.zheng@intel.com>
    Reviewed-by: Hongjiang A Zheng <hongjiang.a.zheng@intel.com>
    Reviewed-by: WeiX Meng <weix.meng@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-15122
    Signed-off-by: Yang Fan F <fan.f.yang@intel.com>
    Reviewed-on: https://android.intel.com:443/455607

--- drivers/external_drivers/camera/drivers/media/pci/atomisp2/atomisp_driver_css15/atomisp_fw.h
+++ drivers/external_drivers/camera/drivers/media/pci/atomisp2/atomisp_driver_css15/atomisp_fw.h
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2010 - 2015, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+**/
+
 /* this is the ISP firmware data running on Merrifield VP */
 char isp2400_fw_data_sim[] = {
 0x8e, 0x02, 0x33, 0x01, 0x14, 0x00, 0x00, 0x00, 0x0c, 0x00, 0x00, 0x00, 0x00,
