a0bef0fa4cee45930e1a74dc72aeee6d31d5d50d
Thulasimani,Sivakumar <sivakumar.thulasimani@intel.com>
2016-01-11 11:23:22 +0530

    FOR_UPSTREAM [VPG]: drm/i915: Update limits for CHV PLL
    VCO limits for calculating the various values required
    in programming PLL has been changed from range of 4.86-6.48
    to 5-6.481. This is updated in this patch.
    
    Change-Id: I8f5b344578c143db9f681ff5886ef606497870a7
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-14849
    Signed-off-by: Sivakumar Thulasimani <sivakumar.thulasimani@intel.com>
    Reviewed-on: https://android.intel.com:443/468191

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -483,7 +483,7 @@ static const intel_limit_t intel_limits_chv = {
 	 * them would make no difference.
 	 */
 	.dot = { .min = 25000 * 5, .max = 540000 * 5},
-	.vco = { .min = 4860000, .max = 6480000 },
+	.vco = { .min = 5000000, .max = 6481000 },
 	.n = { .min = 1, .max = 1 },
 	.m1 = { .min = 2, .max = 2 },
 	.m2 = { .min = 24 << 22, .max = 175 << 22 },
@@ -6890,7 +6890,7 @@ static void chv_update_pll(struct intel_crtc *crtc)
 		loopfilter |= (0xB << DPIO_CHV_INT_COEFF_SHIFT);
 		loopfilter |= (0x3 << DPIO_CHV_GAIN_CTRL_SHIFT);
 		tribuf_calcntr = 0x9;
-	} else if (vco <= 6480000) {
+	} else if (vco <= 6481000) {
 		loopfilter |= (0x4 << DPIO_CHV_PROP_COEFF_SHIFT);
 		loopfilter |= (0x9 << DPIO_CHV_INT_COEFF_SHIFT);
 		loopfilter |= (0x3 << DPIO_CHV_GAIN_CTRL_SHIFT);
