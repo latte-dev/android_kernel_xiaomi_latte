6aa5443a5009501fe78f66eb8e76a92bb6030649
Thakur, Santosh Krx <santosh.krx.thakur@intel.com>
2016-02-04 19:48:26 +0530

    ASoC: Intel: klockwork error in Audio Driver
    This patch helps in copying data from source
    as per the size of destination.
    
    Change-Id: I21908ef78dc23cc3eb34eecacd07fb1f63fc8741
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-18601
    Signed-off-by: Thakur, Santosh Krx <santosh.krx.thakur@intel.com>
    Reviewed-on: https://android.intel.com:443/466574

--- sound/soc/intel/platform-libs/controls_v2_dpcm.c
+++ sound/soc/intel/platform-libs/controls_v2_dpcm.c
@@ -311,8 +311,9 @@ int sst_probe_enum_info(struct snd_kcontrol *kcontrol,
 
 	if (uinfo->value.enumerated.item > e->items - 1)
 		uinfo->value.enumerated.item = e->items - 1;
-	strcpy(uinfo->value.enumerated.name,
-		e->texts[uinfo->value.enumerated.item]);
+	strncpy(uinfo->value.enumerated.name,
+		e->texts[uinfo->value.enumerated.item],
+		sizeof(uinfo->value.enumerated.name));
 	return 0;
 }
 
@@ -373,8 +374,9 @@ int sst_slot_enum_info(struct snd_kcontrol *kcontrol,
 
 	if (uinfo->value.enumerated.item > e->max - 1)
 		uinfo->value.enumerated.item = e->max - 1;
-	strcpy(uinfo->value.enumerated.name,
-		e->texts[uinfo->value.enumerated.item]);
+	strncpy(uinfo->value.enumerated.name,
+		e->texts[uinfo->value.enumerated.item],
+		sizeof(uinfo->value.enumerated.name));
 	return 0;
 }
 
